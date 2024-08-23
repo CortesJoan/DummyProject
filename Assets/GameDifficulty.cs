1bf6645d4cc4cc4b9d6822f25424625e4c66423c
Joan <cortesjoancontact@gmail.com>
2024-08-23 16:55:50 +0200

    Uploaded GameDifficulty scriptable object

---
+++ Assets/GameDifficulty.cs
@@ -0,0 +1,11 @@
+using UnityEngine; 
+
+[CreateAssetMenu(fileName = "New GameDifficulty", menuName = "Game/Difficulty", order = 1)] 
+public class GameDifficulty : ScriptableObject
+{
+    public string difficultyName;
+    public Vector2Int gridSize; 
+    public Color backgroundColor;
+    public int timeToSeeCards;
+}
+ 
