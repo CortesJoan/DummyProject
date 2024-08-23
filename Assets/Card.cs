86187970434e5418390815c0032c966be47f6ac2
Joan <cortesjoancontact@gmail.com>
2024-08-23 13:35:23 +0200

    Fixed minor bug on Card by adding card priority
    -

--- Assets/Card.cs
+++ Assets/Card.cs
@@ -4,9 +4,11 @@ using UnityEngine.UI;
 public class Card : MonoBehaviour
 {
     [SerializeField] private Image cardRenderer;
+    [SerializeField] private LayoutElement layoutElement;
     private Sprite currentSprite;
     private Sprite originalSprite;
-    
+    const int minimumPriority=-1;
+    const int defaultPriority=1;
     public Sprite CurrentSprite
     {
         get { return currentSprite; }
@@ -27,12 +29,16 @@ public class Card : MonoBehaviour
 
     public void RestoreOriginalSprite()
     {
-        cardRenderer.sprite = originalSprite;
+        CurrentSprite= originalSprite;
+        layoutElement.layoutPriority = defaultPriority;
     }
 
     public void ToggleRenderer(bool on)
     {
         cardRenderer.enabled = on;
     }
-
+    public void DownPriority()
+    {
+        layoutElement.layoutPriority = minimumPriority;
+    }
 }
