6aecebf210430e90c8356f68f321ef789da93bb3
Joan <cortesjoancontact@gmail.com>
2024-08-23 17:14:16 +0200

    Created AudioManager to handle all ingame audio

---
+++ Assets/Audio/Scripts/AudioManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioSource bgmAudioSource;
+
+    [Header("Audio clips")]
+    [SerializeField] private AudioClip triesToMatchSound;
+    [SerializeField] private AudioClip matchSound;
+    [SerializeField] private AudioClip mismatchSound;
+
+
+    [SerializeField] private CardMatchUI cardMatchUI;
+
+    private void Start()
+    {
+        cardMatchUI.onMatchMade.AddListener(PlayMatchSound);
+        cardMatchUI.onMatchFailed.AddListener(PlayMismatchSound);
+        cardMatchUI.onTryingMatch.AddListener(PlayTriesToMatchSound);
+    }
+
+
+    private void PlayMatchSound()
+    {
+        audioSource.PlayOneShot(matchSound);
+    }
+
+    private void PlayMismatchSound()
+    {
+        audioSource.PlayOneShot(mismatchSound);
+    }
+
+    private void PlayTriesToMatchSound()
+    {
+        audioSource.PlayOneShot(triesToMatchSound);
+    }
+}
